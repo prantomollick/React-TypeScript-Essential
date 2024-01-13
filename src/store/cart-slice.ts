cebfa6d29383c9ce4dd09e9a954c7a2abad53ae9
Pranto Mollick <prantocse01@gmail.com>
2024-01-13 23:46:38 +0600

    adding login to the reducer function

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type CartItem = {
   id: string;
@@ -19,8 +19,31 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart(state, action) {},
+    addToCart(
+      state,
+      action: PayloadAction<{ id: string; title: string; price: number }>
+    ) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
 
-    removeFromCart() {}
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+    }
   }
 });
